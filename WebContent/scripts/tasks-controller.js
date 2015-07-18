e009c817553f5a100345f8d259a1e7ee1e2b23de
fabricio <fabricio@debian>
2015-07-18 16:14:24 -0300

    Exercicio finalizado

--- WebContent/scripts/tasks-controller.js
+++ WebContent/scripts/tasks-controller.js
@@ -7,10 +7,14 @@ tasksController = function() {
 		console.log(errorCode +':'+ errorMessage);
 	}
 
+	// (1) Criada a função que conta as linhas da tabela para alimentar o id "taskCount"
+	// que está no arquivo 02-tasks.html
 	function taskCounter() {
 		var count = $(taskPage).find('#tblTasks tbody tr').length;
 		storageEngine.findAll('task', function(tasks) {
 			$.each(tasks, function(index, task) {
+				// (4) Essa condicional retira do contador as tarefas que já estão com
+				// a flag 'completo' marcada como true
 				if(task.completo == true){
 					count = count - 1;
 				}
@@ -19,7 +23,12 @@ tasksController = function() {
 		$('footer').find('#taskCount').text(count);
 	}
 
-	function renderTable() {
+	// (3) A função "destacaCriticidade" avalia se a tarefa já está expirada
+	// (caso afirmativo utiliza-se o css 'overdue'), ou se está próximo a data
+	// prevista (como no exercício não estabelece esse prazo, eu estipulei 10 dias)
+	// (caso afirmativo utiliza-se o css 'warning').
+	// Esta função é chamada pelo loadTasks.
+	function destacaCriticidade() {
 		$.each($(taskPage).find('#tblTasks tbody tr'), function(idx, row) {
 			var due = Date.parse($(row).find('[datetime]').text());
 			if(due.compareTo(Date.today()) < 0) {
@@ -63,12 +72,16 @@ tasksController = function() {
 					}
 				);
 
+				// (4) Função 'completeRow' marca a linha com a flag 'completo' e chama novamente o
+				// 'loadTasks'
 				$(taskPage).find('#tblTasks tbody').on('click', '.completeRow',
 					function(evt) {
 						storageEngine.findById('task', $(evt.target).data().taskId, function(task) {
 							task.completo = true;
 							storageEngine.save('task', task, function() {
 								tasksController.loadTasks();
+								// (4) Chama a função 'taskCounter', que conta as linhas da tabela e alimenta
+								// o html, depois da marcação da tarefa completada.
 								taskCounter();
 							},errorLogger);
 						}, errorLogger);
@@ -79,6 +92,8 @@ tasksController = function() {
 					function(evt) { 					
 						storageEngine.delete('task', $(evt.target).data().taskId, function() {
 							$(evt.target).parents('tr').remove();
+							// (2) Chama a função 'taskCounter', que conta as linhas da tabela e alimenta
+							// o html, depois da deleção da linha.
 							taskCounter();
 						}, errorLogger);
 					}
@@ -102,6 +117,12 @@ tasksController = function() {
 		loadTasks : function() {
 			$(taskPage).find('#tblTasks tbody').empty();
 			storageEngine.findAll('task', function(tasks) {
+				// (5) Aplicado a função sort conforme solicitado no exercício.
+				// Para tal finalidade foi utilizado o arquivo jquery 'jquery.dataTables.min.js',
+				// que também aplicou na página as funções "Show - entries" que restringe
+				// a quantidade de linhas na página;  "Search" que faz pesquisa dinâmica pelo
+				// conteúdo das linhas da tabela;  A navegação "previous"/"next"; E uma função
+				// que demonstra a quantidade de páginas processadas
 				$(document).ready(function() {
 					$('#tblTasks').dataTable( {
 						"order": [[ 1, "desc" ]]
@@ -109,12 +130,18 @@ tasksController = function() {
 				} );
 				$.each(tasks, function(index, task) {
 					if (task.id) {
+						// (4) Caso a tarefa não tenha a flag 'completo' alimentada o sistema
+						// marca como false.
 						if (!task.completo) {
 							task.completo = false;
 						}
 						$('#taskRow').tmpl(task).appendTo( $(taskPage).find( '#tblTasks tbody'));
+						// (2) Chama a função 'taskCounter', que conta as linhas da tabela e alimenta
+						// o html.
 						taskCounter();
-						renderTable();
+						// (3) Chama a função 'destacaCriticidade' que altera o css da linha da 
+						// tabela, conforme orientado no enunciado do exercício.
+						destacaCriticidade();
 					}
 				});
 			}, 
--- WebContent/scripts/tasks-indexeddb.js
+++ WebContent/scripts/tasks-indexeddb.js
@@ -1,175 +1,265 @@
-storageEngine = function(){
+// (6) Substituição do arquivo de controle do indexedDB para pleno funcionamento
+
+storageEngine = function() {
+
 	var database;
 	var objectStores;
+
+	function getCount(type, successCallback, errorCallback) {
+		if (!database) {
+			errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
+		}
+
+		var tx = database.transaction(type);
+		var objectStore = tx.objectStore(type);
+		var cursor = objectStore.openCursor();
+		var count = objectStore.count();
+
+		count.onsuccess = function() {
+			successCallback(count.result);
+		}
+
+		count.onerror = function() {
+			errorCallback('record_count_failed', 'The request for the number of stored records has failed');
+		}
+
+	};
+
 	return {
-		init : function(successCallback, errorCallback){
-			if(window.indexedDB){
-				var request = indexedDB.open(window.location.hostname+'DB', 1);
-				request.onsuccess = function(event){
+		init: function(successCallback, errorCallback) {
+
+			if (window.indexedDB) {
+				var request = indexedDB.open(window.location.hostname+'DB');
+
+				request.onsuccess = function(event) {
 					database = request.result;
 					successCallback(null);
 				}
-				request.onerror = function(event){
+				request.onerror = function(event) {
 					errorCallback('storage_not_initialized', 'It is not possible to initialize storage');
 				}
-			}
-			else{
+
+			} else {
 				errorCallback('storage_api_not_supported', 'The web storage api is not supported');
 			}
 		},
-		initObjectStore : function(type, successCallback, errorCallback){
-			if(!database){
+
+		initObjectStore: function(type, successCallback, errorCallback) {
+
+			if (!database) {
 				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
 			}
+
 			var exists = false;
-			$.each(database.objectStoreNames, function(i,v){
-				if(v == type){
+
+
+			$.each(database.objectStoreNames, function(i, v) {
+
+				if (v == type) {
 					exists = true;
 				}
 			});
-			if(exists){
+
+			if (exists) {
+
+				if (!recordCounter.contains(type)) {
+					getCount(type, function(count) {
+						recordCounter.initObjectStoreCount(type, count);
+					});
+				}
+
 				successCallback(null);
-			}
-			else{
-				var version = database.version+1;
+			} else {
+				var version = database.version + 1;
 				database.close();
+
 				var request = indexedDB.open(window.location.hostname+'DB', version);
-				request.onsuccess = function(event){
+
+				request.onsuccess = function(event) {
+					recordCounter.initObjectStoreCount(0);
 					successCallback(null);
 				}
-				request.onerror = function(event){
+
+				request.onerror = function(event) {
 					errorCallback('storage_not_initialized', 'It is not possible to initialize storage');
 				}
-				request.onupgradeneeded = function(event){
+
+				request.onupgradeneeded = function(event) {
 					database = event.target.result;
-					var objectStore = database.createObjectStore(type,{keypath: "id", autoIncrement: true});
+
+					var objectStore = database.createObjectStore(type, {keyPath: 'id', autoIncrement: true});
 				}
 			}
 		},
-		save : function(type, obj, successCallback, errorCallback){
-			if(!database){
+
+		save: function(type, obj, successCallback, errorCallback) {
+
+			if (!database) {
 				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
 			}
-			if(!obj.id){
+
+			if (!obj.id) {
 				delete obj.id;
-			}
-			else{
+			} else {
 				obj.id = parseInt(obj.id);
 			}
-			var tx = database.transaction([type], "readwrite");
-			tx.oncomplete = function(event){
+
+			var tx = database.transaction([type], 'readwrite');
+
+			tx.oncomplete = function(event) {
+				recordCounter.increment(type);
 				successCallback(obj);
 			};
-			tx.onerror = function(event){
+
+			tx.onerror = function(event) {
 				errorCallback('transaction_error', 'It is not possible to store the object');
 			};
+
 			var objectStore = tx.objectStore(type);
 			var request = objectStore.put(obj);
-			request.onsuccess = function(event){
+			request.onsuccess = function(event) {
 				obj.id = event.target.result;
-			};
-			request.onerror = function(event){
+			}
+
+			request.onerror = function(event) {
 				errorCallback('object_not_stored', 'It is not possible to store the object');
 			};
 		},
-		saveAll : function(type, objs, successCallback, errorCallback){
-			if(!database){
+
+		saveAll: function(type, objs, successCallback, errorCallback) {
+
+			if (!database) {
 				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
 			}
-			var tx = database.transaction([type], "readwrite");
-			tx.oncomplete = function(event){
+
+			var tx = database.transaction([type], 'readwrite');
+
+			tx.oncomplete = function(event) {
 				successCallback(objs);
 			};
-			tx.onerror = function(event){
+
+			tx.onerror = function(event) {
 				errorCallback('transaction_error', 'It is not possible to store the object');
 			};
+
 			var objectStore = tx.objectStore(type);
-			$.each(objs, function(indx, obj){
-				if(!obj.id){
+
+			$.each(objs, function(indx, obj) {
+				if (!obj.id) {
 					delete obj.id;
-				}
-				else{
+				} else {
 					obj.id = parseInt(obj.id);
 				}
+
 				var request = objectStore.put(obj);
-				request.onsuccess = function(event){
+
+				request.onsuccess = function(event) {
+					recordCounter.increment(type);
 					obj.id = event.target.result;
 				};
-				request.onerror = function(event){
+
+				request.onerror = function(event) {
 					errorCallback('object_not_stored', 'It is not possible to store the object');
 				};
-			};
-		},
-		findAll : function(type, successCallback, errorCallback){
-			if(!database){
-				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
-			}
-			var result = [];
-			var tx = database.transaction(type);
-			var objectStore = tx.objectStore(type);
-			objectStore.openCursor().onsuccess = function(event){
-				var cursor = event.target.result;
-				if(cursor){
-					result.push(cursor.value);
-					cursor.continue();
-				}
-				else{
-					successCallback(result);
-				}
-			};
-		},
-		delete : function(type, id, successCallback, errorCallback){
-			var obj = {};
-			obj.id = id;
-			var tx = database.transaction([type], "readwrite");
-			tx.oncomplete = function(event){
-				successCallback(id);
-			};
-			tx.onerror = function(event){
-				console.log(event);
-				errorCallback('transaction_error', 'It is not possible to store the object');
-			};
-			var objectStore = tx.objectStore(type);
-			var request = objectStore.delete(id);
-			request.onsuccess = function(event){};
-			request.onerror = function(event){
-				errorCallback('object_not_stored', 'It is not possible to delete the object');
-			};
-		},
-		findByProperty : function(type, propertyName, successCallback, errorCallback){
-			if(!database){
-				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
-			}
-			var result = [];
-			var tx = database.transaction(type);
-			var objectStore = tx.objectStore(type);
-			objectStore.openCursor().onsuccess = function(event){
-				var cursor = event.target.result;
-				if(cursor){
-					if(cursor.value[propertyName] == propertyValue){
-						result.push(cursor.value);
-					}
-					cursor.continue();
-				}
-				else{
-					successCallback(result);
-				}
-			};
+			});
 		},
 
-		findById : function(type, id, successCallback, errorCallback){
-			if(!database){
-					errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
+		findAll: function(type, successCallback, errorCallback) {
+
+			if (!database) {
+				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
 			}
+
+			var result = [];
+
 			var tx = database.transaction(type);
 			var objectStore = tx.objectStore(type);
+			objectStore.openCursor().onsuccess = function(event) {
+
+				var cursor = event.target.result;
+
+				if (cursor) {
+					result.push(cursor.value);
+					cursor.continue();
+				} else {
+					successCallback(result);
+				}
+			}
+		},
+
+		delete: function(type, id, successCallback, errorCallback) {
+
+			var obj = {};
+			obj.id = id;
+
+			var tx = database.transaction([type], 'readwrite');
+			tx.oncomplete = function(event) {
+				recordCounter.decrement(type);
+				successCallback(id);
+			}
+
+			tx.onerror = function(event) {
+				errorCallback('transaction_error', 'It is not possible to store the object');
+			}
+
+			var objectStore = tx.objectStore(type);
+			var request = objectStore.delete(id);
+
+			request.onsuccess = function(event) {
+
+			}
+			request.onerror = function(event) {
+				errorCallback('object_not_stored', 'It is not possible to delete the object');
+			}
+		},
+
+		findByProperty: function(type, propertyName, propertyValue, successCallback, errorCallback) {
+
+			if (!database) {
+				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
+			}
+
+			var result = [];
+			var tx = database.transaction(type);
+			var objectStore = tx.objectStore(type);
+			objectStore.openCursor().onsuccess = function(event) {
+
+				var cursor = event.target.result;
+
+				if (cursor) {
+					if (cursor.value[propertyName] == propertyValue) {
+						result.push(cursor.value);
+					}
+
+					cursor.continue();
+				} else {
+					successCallback(result);
+				}
+			};
+
+		},
+
+		findById: function(type, id, successCallback, errorCallback) {
+
+			if (!database) {
+				errorCallback('storage_api_not_initialized', 'The storage engine has not been initialized');
+			}
+
+			var tx = database.transaction([type]);
+			var objectStore = tx.objectStore(type);
 			var request = objectStore.get(id);
-			request.onsuccess = function(event){
+
+			request.onsuccess = function(event) {
 				successCallback(event.target.result);
-			};
-			request.onerror = function(event){
-				errorCallback('object_not_stored', 'It is not possible to locate the requested object')
-			};
+			}
+
+			request.onerror = function(event) {
+				errorCallback('object_not_stored', 'It is not possible to locate the reqested object');
+			}
+		},
+
+		count: function(type) {
+			return recordCounter.getCount(type);
 		}
 	}
 }();
